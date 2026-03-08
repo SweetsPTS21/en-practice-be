d6bea6eef8f707a7ae75ce0dc0a2ee0e607ddf74
Pham Thanh Son <pts.28032001@gmail.com>
2026-03-08 10:14:13 +0700

    update config

--- src/main/java/com/swpts/enpracticebe/config/FirebaseConfig.java
+++ src/main/java/com/swpts/enpracticebe/config/FirebaseConfig.java
@@ -8,41 +8,33 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
+import java.util.Base64;
 
 @Configuration
 @Slf4j
 public class FirebaseConfig {
 
-    @Value("${firebase.config.file}")
-    private String firebaseConfigPath;
+    @Value("${firebase.config.base64}")
+    private String firebaseConfigBase64;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        if (!FirebaseApp.getApps().isEmpty()) {
-            return FirebaseApp.getInstance();
-        }
+        byte[] decoded = Base64.getDecoder().decode(firebaseConfigBase64);
 
-        InputStream serviceAccount =
-                new ClassPathResource(firebaseConfigPath).getInputStream();
-
-        GoogleCredentials credentials = GoogleCredentials
-                .fromStream(serviceAccount)
-                .createScoped(List.of("https://www.googleapis.com/auth/firebase.messaging"));
+        GoogleCredentials credentials =
+                GoogleCredentials.fromStream(new ByteArrayInputStream(decoded));
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(credentials)
                 .build();
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-        }
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
 
         log.info("Firebase application has been initialized successfully");
-        return FirebaseApp.getInstance();
+        return app;
     }
 
     @Bean
